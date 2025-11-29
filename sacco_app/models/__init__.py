f6d5902655701f111e16aad7ea4b3895f7192a2f
kariokojim <36664062+kariokojim@users.noreply.github.com>
2025-11-29 14:30:17 +0300

    Update __init__.py

--- sacco_app/models/__init__.py
+++ sacco_app/models/__init__.py
@@ -2,6 +2,7 @@ from .member import Member,SaccoAccount, generate_account_number
 from .transactions import Transaction,GLAccount,GLTransaction
 from .loan import Loan, LoanGuarantor, LoanSchedule, LoanInterest
 from .user import User
+from .audit import AuditLog
 
 
 # import all other models here
